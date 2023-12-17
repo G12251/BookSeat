00403d5f438ad5c1ca238194b4fcf8e4a84a0a85
G12251 <gulnozasaparmatova@gmail.com>
2023-12-17 21:55:21 +0500

    Front end

--- server/server.js
+++ server/server.js
@@ -2,4 +2,9 @@ const express = require('express');
 const app = express();
 const port= process.env.PORT || 5000;
 
+
+
+
 app.listen(port, ()=> console.log(`Node JS server is running on port ${port}`));
+
+// Path: server/server.js
