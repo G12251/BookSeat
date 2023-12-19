cce41d2e4216d69de731a2ab2ab66b6b55d4a0e8
G12251 <gulnozasaparmatova@gmail.com>
2023-12-19 17:07:44 +0500

    mongodb

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.mongo_url);
 
 const connection = mongoose.connection;
@@ -11,19 +11,3 @@ connection.on("connected", () => {
 connection.on("error", (err) => {
   console.log("Mongo DB Connection failed");
 });
-
-// const mongoose = require("mongoose");
-
-// mongoose.connect(process.env.mongo_url, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// const connection = mongoose.connection;
-
-// connection.on("connected", () => {
-//   console.log("Mongo DB connection successful");
-// });
-
-// connection.on("error", (err) => {
-//   console.error("Mongo DB connection failed:", err);
-// });
-
-// module.exports = connection;
