6457af18088badcf573d526363c31a7a06c752e1
G12251 <gulnozasaparmatova@gmail.com>
2024-01-12 09:53:27 +0500

    register and login API

---
+++ server/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+constuserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
